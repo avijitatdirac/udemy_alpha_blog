581c01949df5ea4b4fd145c24d3e60ac91fb8370
Rails Project By Me <avijit.chattopadhyay@gmail.com>
2018-03-07 09:20:08 +0000

    Final commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -68,8 +68,8 @@ class ArticlesController < ApplicationController
     
     def require_same_user
       
-      if current_user != @article.user
-        flash[:danger] = "You can only edit/delete/update your own article"
+      if current_user != @article.user and !current_user.admin?
+        flash[:danger] = "You can only edit/delete/update your own article or if you are an admin"
         redirect_to root_path
       end
       
